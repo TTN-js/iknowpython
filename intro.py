8f3aadd46b3d184c5455f2519d5a667fd55c3ec2
ttn_js <ttn.js001@gmail.com>
2017-09-22 16:12:44 -0500

    intro to python

---
+++ intro.py
@@ -0,0 +1,15 @@
+print "Hello, World!"
+
+'''
+Python - a general purpose, object oriented, high level  programing language. Designed to be highly readable.
+Created by Guido van Rossum during 1985 - 1990
+==================================================
+Python is Interpreted: processed at runtime via interprter, no need to compile.
+
+Python is Interactive: play with Python prompt, and interact with the interpreter directly.
+
+Python is Object Oriented: supports OOP aka programing technique that encapsulates code within objects.
+
+Python is a Beginner's Language: Great for beginners!
+==================================================
+'''
